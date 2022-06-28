3d1ea098681327e3fb77c5cefbc36973ffadea75
vaibhav bhardwaj <47694376+BullHoN@users.noreply.github.com>
2022-06-28 12:05:53 +0530

    updated route for adding new menu item

--- routes/partner_routes/menu_items/updateShopItem.js
+++ routes/partner_routes/menu_items/updateShopItem.js
@@ -4,24 +4,34 @@ const router = express.Router();
 
 router.post('/partner/update/menuitem',async (req,res)=>{
 
-    const {shopItemsId, categoryName} = req.query;
+    const {shopItemsId, categoryName, isNewItem} = req.query;
     const shopItemData = req.body; 
+
+    console.log(shopItemsId, categoryName, isNewItem);
+    console.log(shopItemData);
 
     try {
         const shopItems = await ShopItem.findOne({_id: shopItemsId});
+            
         for(let i=0;i<shopItems.categories.length;i++){
             const category = shopItems.categories[i];
             if(category.categoryName == categoryName){
 
-                for(let j=0;j<category.shopItemDataList.length;j++){
-                    const itemName = category.shopItemDataList[j].name;
-                    if(itemName == shopItemData.name){
-                        shopItems.categories[i].shopItemDataList[j] = shopItemData;
-
-                        await shopItems.save();
-                        break;
+                if(isNewItem == 'true'){
+                    shopItems.categories[i].shopItemDataList = [...shopItems.categories[i].shopItemDataList, shopItemData]
+                    await shopItems.save();
+                }
+                else {
+                    for(let j=0;j<category.shopItemDataList.length;j++){
+                        const itemName = category.shopItemDataList[j].name;
+                        if(itemName == shopItemData.name){
+                            shopItems.categories[i].shopItemDataList[j] = shopItemData;
+    
+                            await shopItems.save();
+                            break;
+                        }
+    
                     }
-
                 }
 
                 break;
