d2ef5812c80676343c64f25f4ef2de115cbb976c
vaibhav bhardwaj <47694376+BullHoN@users.noreply.github.com>
2022-07-05 12:59:49 +0530

    added feature: get orders by date

--- routes/partner_routes/orders/getOrders.js
+++ routes/partner_routes/orders/getOrders.js
@@ -5,10 +5,16 @@ const router = express.Router();
 router.get('/partner/getOrders',async (req,res,next)=>{
 
     try {
-        const {shopId, shopCategory, orderStatus, pageNumber, pageSize} = req.query;
+        const {shopId, shopCategory, orderStatus, ordersDateString,  pageNumber, pageSize} = req.query;
+        const greaterThanDate = new Date(ordersDateString)
+        const lessThanDate = new Date(greaterThanDate.getFullYear(),greaterThanDate.getMonth(),greaterThanDate.getDate()+1)
+
+
         // TODO: Change this
-        const orders = await Order.find({shopCategory: shopCategory, shopId: shopId, orderStatus: Number.parseInt(orderStatus)})
+        const orders = await Order.find({shopCategory: shopCategory, shopId: shopId, 
+            orderStatus: Number.parseInt(orderStatus), updated_at: { $gte: greaterThanDate, $lt: lessThanDate }})
             .sort({ created_at: 1 });
+
         res.json(orders);
     }
     catch(error){
