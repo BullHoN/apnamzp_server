7be1d144c151f625e755e22897603321c8042ba8
vaibhav bhardwaj <47694376+BullHoN@users.noreply.github.com>
2022-07-29 12:23:18 +0530

    added monthly in partners

--- routes/partner_routes/orders/getOrders.js
+++ routes/partner_routes/orders/getOrders.js
@@ -6,10 +6,26 @@ const router = express.Router();
 router.get('/partner/getOrders',async (req,res,next)=>{
 
     try {
-        const {shopId, shopCategory, orderStatus, ordersDateString,  pageNumber, pageSize} = req.query;
-        const greaterThanDate = new Date(ordersDateString)
-        const lessThanDate = DF.addDays(greaterThanDate,1)
+        const {shopId, shopCategory, orderStatus, ordersDateString,  pageNumber, pageSize, isMonthly} = req.query;
 
+        let greaterThanDate;
+        let lessThanDate;
+    
+        if(isMonthly == "true"){
+            const curr_year = Number.parseInt(ordersDateString.split('-')[0])
+            const curr_month = Number.parseInt(ordersDateString.split('-')[1])
+    
+            greaterThanDate = new Date(curr_year,curr_month-1)
+            greaterThanDate = DF.add(greaterThanDate, {days: 1})
+    
+            lessThanDate = DF.add(greaterThanDate, {
+                months: 1
+            })
+        }
+        else {
+            greaterThanDate = new Date(ordersDateString)
+            lessThanDate = DF.addDays(greaterThanDate,1)  
+        }
 
         // TODO: Change this
         const orders = await Order.find({shopCategory: shopCategory, shopId: shopId, 
