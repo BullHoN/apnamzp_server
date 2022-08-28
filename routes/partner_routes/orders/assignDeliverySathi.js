3126351aa4a9a8fdf0b014f8ec87fbc3d6ee0f96
vaibhav bhardwaj <47694376+BullHoN@users.noreply.github.com>
2022-08-28 23:27:06 +0530

    axios redirection fixed

--- routes/partner_routes/orders/assignDeliverySathi.js
+++ routes/partner_routes/orders/assignDeliverySathi.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const PartnerOrder = require('../../../controllers/partner/partner.order.controller')
 
-router.post('/partner/assignDeliveryBoy',PartnerOrder.assignDeliveryBoy);
-
+router.post('/partner/assignDeliveryBoy', PartnerOrder.assignDeliveryBoy);
+router.get('/partner/assignDeliveryBoy', PartnerOrder.assignDeliveryBoy)
 
 module.exports = router;
