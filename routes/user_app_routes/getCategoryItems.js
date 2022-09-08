f10ac38d1d0d8eaa62f281805cc525eec927759c
vaibhav bhardwaj <47694376+BullHoN@users.noreply.github.com>
2022-09-08 16:59:11 +0530

    show available shops first

--- routes/user_app_routes/getCategoryItems.js
+++ routes/user_app_routes/getCategoryItems.js
@@ -8,12 +8,12 @@ router.get('/category/:shopType',async (req,res,next)=>{
     try{
         if(req.params.shopType == "all"){
             const data =  await Shop.find({showShop: true})
-                .sort({averageRatings: -1});
+                .sort({isOpen: 1,averageRatings: -1});
             res.json(data);
         }
         else {
             const data =  await Shop.find({shopType: req.params.shopType,showShop: true})
-                .sort({averageRatings: -1});
+                .sort({isOpen: -1,averageRatings: -1});
             res.json(data);
         }
     }
