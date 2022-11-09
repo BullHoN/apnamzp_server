a37683650d29faebe904daca86b9285d31f7af94
vaibhav bhardwaj <47694376+BullHoN@users.noreply.github.com>
2022-11-09 16:04:41 +0530

    alert fix

--- alerts/resturant_not_responded.alert.js
+++ alerts/resturant_not_responded.alert.js
@@ -11,7 +11,7 @@ function resturantNotRespondedAlert(orderId,adminShopService){
     const order = await Order.findOne({_id: orderId})
     const shopPartner = await ShopPartner.findOne({shopId: order.shopID})
     
-    if(order.orderStatus != 0){
+    if(order.orderStatus > 0){
         return;
     }
 
